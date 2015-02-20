34e0443d6e2d72d6f374b5edfb01c22d657372bf
jasmo2 <jasmo2.com@gmail.com>
2015-02-20 07:27:41 -0500

    grab web content for Forecast

--- ¿Weather?/ViewController.swift
+++ ¿Weather?/ViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cityText: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    @IBAction func findWeatherButton(sender: AnyObject) {
+    }
+    func showError(){
+        resultLabel.text = "The city: \(cityText.text) you input was not found"
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+      var url =  NSURL(string: "http://www.weather-forecast.com/locations/Bogota/forecasts/latest")
+        if true{
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!){
+                (data,response,error) in
+                
+                if error == nil{
+                    var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding) as NSString!
+                    var urlContentArray = urlContent.componentsSeparatedByString("<p class=\"summary\">")
+                    println(urlContentArray[1])
+                }
+                else{
+                    self.showError()
+                };
 
+                }
+            task.resume()
+    
+        }
+    }
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        return UIInterfaceOrientation.Portrait.rawValue
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
 
